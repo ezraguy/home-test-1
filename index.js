01117250456b403fea7ff18935aded0bf8932d9f
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-12-11 11:26:57 +0200

    adding form

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const addFixedClass = () => {
     const sidePanel = document.getElementById('side-pan');
-    console.log('s');
+
     window.addEventListener('scroll', () => {
         if (pageYOffset >= 270)
             sidePanel.classList = 'side-panel fixed'

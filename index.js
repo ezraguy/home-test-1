6100f09dfe6a31377c79b8fa79aea5b4b8bde554
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-12-11 13:28:50 +0200

    ui changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const addFixedClass = () => {
 
 
 const validateForm = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
 
     const inputs = document.getElementById('form').elements;
     const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;

addf9982d861144f17a82da40c9b20ca3b48c7b0
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-12-11 14:34:09 +0200

    changing const and let to var for ie support

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function addFixedClass() {
-    const sidePanel = document.getElementById('side-pan');
+    var sidePanel = document.getElementById('side-pan');
     window.addEventListener('scroll', function () {
         if (pageYOffset >= 270)
             sidePanel.className = 'side-panel fixed ';
@@ -12,12 +12,12 @@ function addFixedClass() {
 
 function validateForm(e) {
     e.preventDefault();
-    const inputs = document.getElementById('form').elements;
-    const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    const emailErrorMessage = document.getElementById('email-err-msg');
-    let canSubmit = true;
-    for (let i = 0; i < inputs.length; i++) {
-        const element = inputs[i];
+    var inputs = document.getElementById('form').elements;
+    var emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    var emailErrorMessage = document.getElementById('email-err-msg');
+    var canSubmit = true;
+    for (var i = 0; i < inputs.length; i++) {
+        var element = inputs[i];
         if (element.value.length === 0) {
             element.className = 'empty-input';
             canSubmit = false;
